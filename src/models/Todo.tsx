f6304e2814084e30d0320145d86e426795e1a8a0
jakkrid.wi <jakkrid.wi@gmail.com>
2025-07-14 20:49:05 +0700

    update todo model

--- src/models/Todo.tsx
+++ src/models/Todo.tsx
@@ -5,29 +5,3 @@ export interface Todo {
   createdAt: Date;
   updatedAt: Date;
 }
-
-export class TodoModel implements Todo {
-  id: number | string;
-  text: string;
-  completed: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-
-  constructor(text: string, id?: number | string) {
-    this.id = id ?? Date.now(); 
-    this.text = text;
-    this.completed = false;
-    this.createdAt = new Date();
-    this.updatedAt = new Date();
-  }
-
-  toggleCompleted() {
-    this.completed = !this.completed;
-    this.updatedAt = new Date();
-  }
-
-  updateText(newText: string) {
-    this.text = newText;
-    this.updatedAt = new Date();
-  }
-}
